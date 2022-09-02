7fdf749911c78de24d95c630a599cfa9fb260b39
Alan Haynes <aghaynes@users.noreply.github.com>
2022-09-02 07:33:43 +0200

    option to not ignore anything

--- R/remove_empty_rows.R
+++ R/remove_empty_rows.R
@@ -7,10 +7,18 @@
 #' @export
 #'
 #' @examples
-#' x <- data.frame(a = 1:10, b = rep(c("b", NA), 5))
+#' x <- data.frame(a = c(1:9, NA), b = rep(c("b", NA), 5))
 #' remove_empty_rows(x, "a")
+#' remove_empty_rows(x, FALSE)
 remove_empty_rows <- function(data, ignore = "^(record_id|redcap)|_complete$"){
-  tmp <- data[, !grepl(ignore, names(data)), drop = FALSE]
+  if(is.logical(ignore)){
+    if(ignore) stop("'ignore should be FALSE or a regex string'")
+    ignore_vars <- rep(TRUE, ncol(data))
+  } else {
+    if(ignore == "") warning('ignore = "" will ignore everything')
+    ignore_vars <- !grepl(ignore, names(data))
+  }
+  tmp <- data[, ignore_vars, drop = FALSE]
   mask_keep <- rowSums(is.na(tmp) | tmp == "") != ncol(tmp)
   data[mask_keep, , drop = FALSE]
 }
