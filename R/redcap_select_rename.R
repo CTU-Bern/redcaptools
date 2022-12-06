8b0a054aa238cc778491389cb2b5fe6bffea6b2a
Güntensperger <dg20s066@campus.unibe.ch>
2022-12-06 15:54:24 +0100

    redcap_select_rename finalized with test data, description updated

--- R/redcap_select_rename.R
+++ R/redcap_select_rename.R
@@ -11,39 +11,52 @@
 #'
 #'
 #'@param import_data Data frame to be imported
+#'@param dict Data dictionary (e.g. as
+#'  downloaded from REDCap or via \code{redcap_export_meta(rc_token,
+#'  rc_url)$meta}). If not supplied, this will be downloaded from the API using
+#'  \code{rc_token} and \code{rc_url}.
 #'@param rc_token REDCap API token
-#'@param url Link to REDCap API. Default: https://redcap.ctu.unibe.ch/api/
-#'@param dict Data dictionary (e.g. as downloaded from REDCap or via
-#'  \code{redcap_export_meta(rc_token, url)$meta}). If not supplied, this will
-#'  be downloaded from the API using \code{rc_token}.
+#'@param rc_url Link to REDCap API.
 #'@param forms List of REDCap forms of which variable names will be displayed.
 #'  Default = all forms.
+#'@param auto if TRUE, variables with matching names will be automatically
+#'  selected. If FALSE, the user can decide if the variable shall be imported or
+#'  not. Default = TRUE.
 #'
 #'@return Data frame with selected/renamed data. Log-file with executed code.
+#'  CSV-table with overview.
 #'@export
 #'@importFrom stringr str_detect
-#'@importFrom crayon bold underline
+#'@importFrom crayon bold underline blue italic
 #'@importFrom dplyr select filter
 #'
 #' @examples
-#' token <- "xxxxx"
-#' file <- "inst/data.csv"
-#' redcap_select(file, token)
+#' data(importdemo)
+#' data(meta)
+#' redcap_select_rename(importdemo, meta)
+#'
+#' # if using local data:
+#' # token <- "xxxxx"
+#' # url <- "xxxxx"
+#' # file <- "data.csv"
+#' # redcap_recode(file, rc_token = token, rc_url = url)
+
 
 
 
 redcap_select_rename <- function(import_data,
-                                 rc_token,
-                                 url = "https://redcap.ctu.unibe.ch/api/",
                                  dict = NULL,
-                                 forms = NULL) {
+                                 rc_token,
+                                 rc_url,
+                                 forms = NULL,
+                                 auto = TRUE) {
 
 
   # load data
 
   imp_vars <- colnames(import_data)
 
-  if(is.null(dict)) dict <- redcap_export_meta(rc_token, url)$meta
+  if(is.null(dict)) dict <- redcap_export_meta(rc_token, rc_url)$meta
 
   if(!is.null(forms)) dict <- filter(dict, form_name %in% forms)
   rc_vars <- select(dict,c(field_name,field_label))
@@ -72,87 +85,84 @@ redcap_select_rename <- function(import_data,
 
 
   for (i in seq_along(imp_vars)) {
-
-    ans <- 3
-    if (any(str_detect(paste0("^",rc_vars$field_name,"$"),imp_vars[i]))) {                                   # if variable name found in REDCap dictionnary
-      cat(paste("Variable name found in REDCap:", bold(underline(imp_vars[i]))))
-      cat("\nShould the variable be kept with this name? \n 1 = YES\n 0 = NO \n 'exit' = stop loop")
-      ans <- 3
-      while (ans != 1 && ans != 0 && ans != 'exit') {
-        ans <- readline(prompt="Answer= ")
-        if (ans != 1 && ans != 0 && ans != 'exit') {
-          cat("Please check your answer! \n 1 = YES\n 0 = NO \n 'exit' = stop loop")
+    ans <- ""
+    if (any(str_detect(rc_vars$field_name,paste0("^",imp_vars[i],"$")))) {                                    # if variable name found in REDCap dictionnary
+      cat(paste("Variable name found in REDCap:", blue(bold(underline(imp_vars[i])))))
+      if (!auto) {                                                                                            # if auto-import is set to FALSE
+        cat("\nShould the variable be kept with this name? \n 1 = YES\n 0 = NO \n 'exit' = stop loop")
+        ans <- ""
+        while (ans != 1 && ans != 0 && ans != 'exit') {
+          ans <- readline(prompt="Answer= ")
+          if (ans != 1 && ans != 0 && ans != 'exit') {
+            cat("Please check your answer! \n 1 = YES\n 0 = NO \n 'exit' = stop loop")
+          }
         }
       }
 
+
       if (ans == 'exit') {
         break
-      }
-
-      if (as.integer(ans) == 1) {
+      } else if ((ans == '1') || auto) {
         vars_rename[[length(vars_rename)+1]] <- imp_vars[i]                                 # import variable without renaming
         imp_vars_nonewname <- c(imp_vars_nonewname,imp_vars[i])
         rc_vars <- filter(rc_vars,field_name != imp_vars[i])
         write.table(paste0(", ",imp_vars[i]), log_file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
-        write.table(imp_vars[i], log_table, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+        write.table(paste("",imp_vars[i], sep = ","), log_table, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+
+        cat(italic("\n\nVariable will be imported without renaming!\n"))
+        cat("\n-----------------------------------------------------------------\n")
+        Sys.sleep(2)
 
         next
       }
     }
 
-    if (!any(str_detect(paste0("^",rc_vars$field_name,"$"),imp_vars[i]))) {                                  # if variable name is not found in REDCap dictionnairy
-      cat(paste("\nVariable name", underline("NOT"),"found in REDCap:", bold(underline(imp_vars[i]))))
+    if (!any(str_detect(rc_vars$field_name,paste0("^",imp_vars[i],"$")))) {                                  # if variable name is not found in REDCap dictionnairy
+      cat(paste("\nVariable name", underline("NOT"),"found in REDCap:", blue(bold(underline(imp_vars[i])))),"\n\n")
+      Sys.sleep(1)
     }
 
-    if (!any(str_detect(paste0("^",rc_vars$field_name,"$"),imp_vars[i])) | as.integer(ans) == 0) {           # if variable name is not found in REDCap dictionnairy or should not be imported with same name
-      cat("\nShould the variable be imported at all? \n 1 = YES\n 0 = NO \n 'exit' = stop loop")
-      ans <- 3
-      while (ans != 1 && ans != 0 && ans != 'exit') {
+    if (!any(str_detect(rc_vars$field_name,paste0("^",imp_vars[i],"$"))) | ans == '0') {           # if variable name is not found in REDCap dictionnairy or should not be imported with same name
+      print(rc_vars)
+      cat(paste("\n\nPlease choose REDCap name from list above for:", blue(bold(underline(imp_vars[i])))),"\n")
+      cat("\n Type the name or choose the respective number!\n 'skip' = do NOT select and move to next item \n 'exit' = do Not select and stop loop \n ")
+
+      ans <- ""
+      while (!any(grepl(paste0("^",ans,"$"),rc_vars$field_name)) && !any(grepl(paste0("^",ans,"$"),rownames(rc_vars))) && ans != 'exit' && ans != 'skip') {
         ans <- readline(prompt="Answer= ")
-        if (ans != 1 && ans != 0 && ans != 'exit') {
-          cat("Please check your answer! \n 1 = YES\n 0 = NO \n 'exit' = stop loop")
+        if (!any(grepl(paste0("^",ans,"$"),rc_vars$field_name)) && !any(grepl(paste0("^",ans,"$"),rownames(rc_vars))) && ans !='exit' && ans != 'skip') {
+          cat("Variable name not recognized: Please try again!\n 'skip' = do NOT select and move to next item \n 'exit' = do Not select and stop loop \n ")
         }
       }
-
-      if (ans == 'exit') {                                                                     # check answers:
-        break                                                                                     # exit
-
-      } else if (as.integer(ans) == 0) {                                                          # do not import variable
+      if (ans == 'exit') {                                                                    # check answers:
+        break                                                                                    # exit
+      } else if (ans == 'skip') {
         imp_vars_out <- c(imp_vars_out,imp_vars[i])
-        next
-
-      } else if (as.integer(ans) == 1) {
-        print(rc_vars)                                                                            # import with renaming
-        cat("Please choose REDCap name from list above for:")
-        cat(paste("\n",bold(underline(imp_vars[i]))))
-        cat("\n Type the name or choose the respective number!\n 'exit' = do Not select and stop loop \n 'skip' = do NOT select and move to next item")
-        ans <- "-999"
-        while (!any(grepl(paste0("^",ans,"$"),rc_vars$field_name)) && !any(grepl(paste0("^",ans,"$"),rownames(rc_vars))) && ans != 'exit' && ans != 'skip') {
-          ans <- readline(prompt="Answer= ")
-          if (!any(grepl(paste0("^",ans,"$"),rc_vars$field_name)) && !any(grepl(paste0("^",ans,"$"),rownames(rc_vars))) && ans !='exit' && ans != 'skip') {
-            cat("Variable name not recognized: Please try again!\n 'exit' = do Not select and stop loop \n 'skip' = do NOT select and move to next item")
-          }
-        }
-        if (ans == 'exit') {                                                                    # check answers:
-          break                                                                                    # exit
-        } else if (ans == 'skip') {
-          imp_vars_out <- c(imp_vars_out,imp_vars[i])
-          next                                                                                     # skip
-        } else if (suppressWarnings(!is.na(as.integer(ans)))) {
-          new_name <- rc_vars$field_name[as.integer(ans)]                                          # nbr has been entered
-        } else {
-          new_name <- ans                                                                          # name has been entered
-        }
-
-        vars_rename[[length(vars_rename)+1]] <- imp_vars[i]
-        names(vars_rename)[length(vars_rename)] <- new_name
-        imp_vars_rename <- c(imp_vars_rename,paste(imp_vars[i],"=",new_name))
-        rc_vars <- filter(rc_vars,field_name != new_name)
-        write.table(paste0(", ",new_name," = ",imp_vars[i]), log_file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
-        write.table(paste(imp_vars[i],new_name, sep = ","), log_table, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+        cat(italic("\nVariable will not be be selected!\n"))
+        cat("\n-----------------------------------------------------------------\n")
+        Sys.sleep(2)
+        next                                                                                     # skip
+      } else if (suppressWarnings(!is.na(as.integer(ans)))) {
+        new_name <- rc_vars$field_name[as.integer(ans)]                                          # nbr has been entered
+      } else {
+        new_name <- ans                                                                          # name has been entered
       }
+
+      vars_rename[[length(vars_rename)+1]] <- imp_vars[i]
+      names(vars_rename)[length(vars_rename)] <- new_name
+      imp_vars_rename <- c(imp_vars_rename,paste(imp_vars[i],"=",new_name))
+      rc_vars <- filter(rc_vars,field_name != new_name)
+      write.table(paste0(", ",new_name," = ",imp_vars[i]), log_file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+      write.table(paste("",imp_vars[i],new_name, sep = ","), log_table, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+
+      cat(italic(paste0("\nVariable will be selected and renamed:\n\n Old Name = ",imp_vars[i],"\n New Name = ",new_name)))
+      cat("\n\n-----------------------------------------------------------------\n")
+      Sys.sleep(2)
+
+
     }
   }
+
 
   # SELECT AND RENAME VARIABLES
   selected_data <- select(import_data, !!!vars_rename)
@@ -169,8 +179,8 @@ redcap_select_rename <- function(import_data,
   write.table(paste("\nThe following Variables have not been selected:"), log_file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
   write.table(paste(imp_vars_out, sep="\n"), log_file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
 
-  write.table("\n--------------------------------------------------------------------------------------------------\n", log_file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
-  write.table("\n", log_table, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+  write.table("\n-----------------------------------------------------------------\n", log_file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+  write.table("\n\n", log_table, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
 
   # Return Output
   cat("ALL DONE!!!")
