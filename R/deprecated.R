9f83ba69dfcb2245364af032e3f2bcdbb4b7bc3d
Alan Haynes <aghaynes@users.noreply.github.com>
2024-03-12 10:42:30 +0100

    deprecate split_by_form

--- R/deprecated.R
+++ R/deprecated.R
@@ -45,3 +45,25 @@ rc_datetimes <- function(data, metadata, replace = FALSE, append = "_datetime", 
   warning("rc_datetimes is deprecated, use redcap_prep_datetimes instead")
   redcap_prep_datetimes(data, metadata, replace, append, ...)
 }
+
+#' @describeIn deprecated deprecated in favour of \code{redcap_toform}
+#' Split a manually exported REDCap dataset into forms
+#'
+#' @param data dataframe
+#' @param metadata datadictionary as exported from REDCap or downloaded from the API
+#'
+#' @return list of dataframes
+#' @export
+split_by_form <- function(data, metadata){
+  warning("split_by_form is deprecated, use redcap_toform instead")
+  metadata$regex <- ifelse(metadata$field_type == "checkbox",
+                           paste0(metadata$field_name, "___"),
+                           metadata$field_name)
+  sapply(unique(metadata$form_name), function(x){
+    regex <- paste(metadata$field_name[1], "^redcap",
+                   paste0("^", metadata$regex[metadata$form_name == x],
+                          collapse = "|"), sep = "|")
+    dd <- data[, grepl(regex, names(data))]
+    remove_empty_rows(dd)
+  })
+}
